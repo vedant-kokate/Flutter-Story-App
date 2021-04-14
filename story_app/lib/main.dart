b9ff64a4cd542d5071046d36668ce3a718aad3c8
vedantkokate07 <vedant.kokate@somaiya.edu>
2021-04-14 23:03:00 +0530

    images with round borders

--- story_app/lib/main.dart
+++ story_app/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'custonIcons.dart';
+import 'data.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -12,8 +15,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var currentPage = images.length - 1.0;
+
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      currentPage = controller.page;
+    });
+
     return Scaffold(
       backgroundColor: Color(0xFF2f3447),
       body: SingleChildScrollView(
@@ -27,7 +37,7 @@ class _MyAppState extends State<MyApp> {
                 children: <Widget>[
                   IconButton(
                     icon: Icon(
-                      Icons.menu,
+                      CustomIcons.menu,
                       color: Colors.white,
                       size: 30.0,
                     ),
@@ -53,21 +63,148 @@ class _MyAppState extends State<MyApp> {
                       'Trending',
                       style: TextStyle(
                           color: Colors.white,
-                          fontSize: 40.0,
+                          fontSize: 46.0,
                           fontFamily: "Calibre-Semibold",
                           letterSpacing: 1.0),
                     ),
                     IconButton(
                         icon: Icon(
                           CustomIcons.option,
-                          size: 30.0,
+                          size: 12.0,
                           color: Colors.white,
                         ),
                         onPressed: () {})
                   ],
+                )),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFff6e6e),
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Center(
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 22.0, vertical: 6.0),
+                          child: Text("Animated",
+                              style: TextStyle(color: Colors.white))),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Text(
+                    "25+ Stories",
+                    style: TextStyle(color: Colors.blueAccent),
+                  )
+                ],
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                CardScrollWidget(currentPage),
+                Positioned.fill(
+                    child: PageView.builder(
+                  itemCount: images.length,
+                  controller: controller,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return Container();
+                  },
                 ))
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+var cardAspectRatio = 12.0 / 16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;
+var verticalInset = 20.0;
+
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  CardScrollWidget(this.currentPage);
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(
+        builder: (context, constrains) {
+          var width = constrains.maxWidth;
+          var height = constrains.maxHeight;
+
+          var safeWidth = width - 2 * padding;
+          var safeHeight = height - 2 * padding;
+
+          var heightOfPrimaryCard = safeHeight;
+          var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+          var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+          var horizontalInset = primaryCardLeft / 2;
+          List<Widget> cardList = new List();
+          for (var i = 0; i < images.length; i++) {
+            var delta = i - currentPage;
+            bool isOnRight = delta > 0;
+            var start = padding +
+                max(
+                    primaryCardLeft -
+                        horizontalInset * -delta * (isOnRight ? 15 : 1),
+                    0.0);
+            var cardItem = Positioned.directional(
+                top: padding + verticalInset * max(-delta, 0.0),
+                bottom: padding + verticalInset * max(-delta, 0.0),
+                start: start,
+                textDirection: TextDirection.rtl,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(3.0, 6.0),
+                          blurRadius: 10.0)
+                    ]),
+                    child: AspectRatio(
+                        aspectRatio: cardAspectRatio,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: <Widget>[
+                            Image.asset(images[i], fit: BoxFit.cover),
+                            Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 16.0, vertical: 8.0),
+                                      child: Text(title[i],
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 25.0,
+                                              fontFamily:
+                                                  "SF-Pro-Text-Regular")),
+                                    )
+                                  ],
+                                ))
+                          ],
+                        )),
+                  ),
+                ));
+            cardList.add(cardItem);
+          }
+          return Stack(
+            children: cardList,
+          );
+        },
       ),
     );
   }
